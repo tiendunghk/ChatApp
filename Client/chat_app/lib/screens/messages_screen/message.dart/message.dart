873ffd49c0d2ffcfb23fb8b289601a3d21c5c767
Dung Nguyen <dunghkuit@gmai.com>
2021-08-02 22:23:26 +0700

    refactor ui

--- Client/chat_app/lib/screens/messages_screen/message.dart/message.dart
+++ Client/chat_app/lib/screens/messages_screen/message.dart/message.dart
@@ -1,6 +1,7 @@
 import 'package:chat_app/constants.dart';
 import 'package:chat_app/models/message.dart' as mes;
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class Message extends StatelessWidget {
   final mes.Message message;
@@ -16,14 +17,30 @@ class Message extends StatelessWidget {
             margin: EdgeInsets.only(top: defaultPadding),
             padding: EdgeInsets.symmetric(
               horizontal: defaultPadding * 0.75,
-              vertical: defaultPadding / 2,
+              vertical: defaultPadding / 3,
             ),
             decoration: BoxDecoration(
                 color: message.isSender
                     ? Color(0xFF2B5278)
                     : Color(0xFF2B5278).withOpacity(0.5),
                 borderRadius: BorderRadius.circular(20)),
-            child: Text(message.messageContent))
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(message.messageContent),
+                SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 12),
+                  child: Text(
+                    DateFormat('hh:MM a').format(message.messageCreatedAt),
+                    style: TextStyle(
+                        color: Colors.white.withOpacity(0.7), fontSize: 10),
+                  ),
+                ),
+              ],
+            ))
       ],
     );
   }
